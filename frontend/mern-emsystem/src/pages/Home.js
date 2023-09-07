842b59a1b5cf4a79568886308e7c2dcd5568c8b1
matthewJarrams <matthew.jarrams@gmail.com>
2023-09-07 15:10:18 -0600

    Branches merged
    Everything seems to be ok. I think...

--- frontend/mern-emsystem/src/pages/Home.js
+++ frontend/mern-emsystem/src/pages/Home.js
@@ -39,19 +39,30 @@ const Home = () => {
   };
 
   return (
-    <div className="home">
-
-      </div>
+      <div className="flex justify-center items-center">
+        <ol className="divide-y divide-gray-100 w-96 " >
+          {notes && notes.map((note) => (
+            <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50 text-left"><a key={note._id} target="blank" onClick={() => handleClick(note)} className=" bg-blue-400 py-2 px-4 rounded text-center hover:bg-blue-800 hover:text-blue-50 cursor-pointer shadow-sm " >{note.StudentName}</a>
+              <div className="py-2 px-4 text-right"> View Notes </div>
+            </li>
+          )
+          )}
+        </ol>
       <div>
-      {notes && notes.map((note) => (
-          
-      <Link
+        {notes && notes.map((note) => (
+          // <li><a key={note._id} target="blank" onClick={() => handleClick(note)}>{note.StudentName}</a></li>
+          <Link
               to={{ pathname: `/studentProfile/${note._id}`, state: {data : note}}}
       >
             note._id
         </Link>
-      ))
-      }
+      
+          
+        )
+        
+      
+        
+        )}
       </div>
     </div>
   )
