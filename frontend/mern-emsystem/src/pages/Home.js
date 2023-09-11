0f7d5d42bfc0a00659617d5b2d0e87183d51ce16
Carter Babin <carterbabin@pop-os.localdomain>
2023-09-11 14:42:58 -0600

    make it say the student name

--- frontend/mern-emsystem/src/pages/Home.js
+++ frontend/mern-emsystem/src/pages/Home.js
@@ -60,7 +60,7 @@ const Home = () => {
               <Link
               to={{ pathname: `/studentProfile/${note._id}`, state: {data : note}}}
       >
-            {note._id}
+            {note.StudentName}
         </Link>
             </li>
           )
